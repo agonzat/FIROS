05f3ac4ca6f25015ab2c07611a9d229941615c7f
agonzat <andergonzaleztome@gmail.com>
2023-06-23 10:48:53 +0000

    Force . syntax

--- firos/include/pubsub/contextbroker/cbPublisher.py
+++ firos/include/pubsub/contextbroker/cbPublisher.py
@@ -96,7 +96,8 @@ class CbPublisher(Publisher):
             self.posted_history[topic] = rawMsg
             
             obj = {s: getattr(rawMsg, s, None) for s in rawMsg.__slots__}
-            obj["type"] = rawMsg._type.replace("/", ".") # OCB Specific!!
+            obj["type"] = rawMsg._type.replace("/", ".")
+            obj["type"] = obj["type"].replace("%2F", ".") # OCB Specific!!
             obj["id"] = (topic).replace("/", ".") # OCB Specific!!
             jsonStr = ObjectFiwareConverter.obj2Fiware(obj, ind=None, dataTypeDict=msgDefintionDict[topic],  ignorePythonMetaData=True, encode=True)
             response = requests.post(self.CB_BASE_URL, data=jsonStr, headers=self.CB_HEADER)
@@ -109,6 +110,7 @@ class CbPublisher(Publisher):
         # Create Update-JSON
         obj = {s: getattr(rawMsg, s, None) for s in rawMsg.__slots__}
         obj["type"] = rawMsg._type.replace("/", ".") # OCB Specific!!
+        obj["type"] = obj["type"].replace("%2F", ".")
         obj["id"] = (topic).replace("/", ".") # OCB Specific!!
         jsonStr = ObjectFiwareConverter.obj2Fiware(obj, ind=None, dataTypeDict=msgDefintionDict[topic],  ignorePythonMetaData=True, showIdValue=False, encode=True) 
         # print(jsonStr)
